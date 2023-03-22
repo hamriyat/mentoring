ae4228d5facc20fcfd0d693bf1d3ce1e24f806bb
hamriyat <vladlahman@gmail.com>
2023-03-22 22:11:04 +0200

    added some js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import './style.scss';
 
+// import Swiper JS
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+let swiper = new Swiper(".my-swiper", {
+    modules: [Navigation, Pagination],
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    slidesPerView: 1,
+    spaceBetween: 30,
+    breakpoints: {
+        767: {
+            slidesPerView: 2,
+        },
+
+        1140: {
+            slidesPerView: 3,
+        }
+    }
+});
+
 console.log('Webpack starter with SCSS and dev server');
